0ede0f875b356213865264a5a95037673764062c
Justin Puuri <justinpuuri@gmail.com>
2021-10-17 17:42:30 -0600

    Add GitHubModel to the project

---
+++ Recipe List/Models/GitHubModel.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubModel.swift
+//  Recipe List
+//
+//  Created by Justin Puuri on 10/17/21.
+//
+
+import Foundation
+
+struct GitHub {
+    var model = "Test for github"
+    
+    func gitHub () {
+        print(model)
+    }
+    
+}
