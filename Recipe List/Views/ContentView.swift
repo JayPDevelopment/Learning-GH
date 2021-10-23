57471849c66f423e6011c068036c209011c9e1b3
Justin Puuri <justinpuuri@gmail.com>
2021-10-23 13:33:59 -0600

    - change swiftUI list to scrollview, lazyvstack and foreach - hide navigationbartitle and use text element for the title in RecipeListView

--- Recipe List/Views/ContentView.swift
+++ Recipe List/Views/ContentView.swift
@@ -15,23 +15,45 @@ struct ContentView: View {
     var body: some View {
         
         NavigationView {
-            List(model.recipes) { r in
+            VStack (alignment: .leading){
                 
-                NavigationLink {
-                    RecipeDetailView(recipe: r)
-                } label: {
-                    HStack(spacing: 20.0) {
-                        Image(r.image)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 50, height: 50, alignment: .center)
-                            .clipped()
-                            .cornerRadius(5)
-                        Text(r.name)
+                Text("All Recipes")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.top, 40)
+                
+                ScrollView {
+                    
+                    // A lazy vstack only creates or renders items as needed
+                    // When you have a ton of rows, you don't want your phone to allocate memory to EVERY row, just the ones being displayed or used
+                    LazyVStack (alignment: .leading) {
+                        
+                        ForEach (model.recipes) { r in
+                            
+                            NavigationLink {
+                                RecipeDetailView(recipe: r)
+                            } label: {
+                                HStack(spacing: 20.0) {
+                                    Image(r.image)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 50, height: 50, alignment: .center)
+                                        .clipped()
+                                        .cornerRadius(5)
+                                    Text(r.name)
+                                        .foregroundColor(Color.black)
+                                }
+                            }
+                        }
                     }
                 }
+                
+                // Here, we're trying to format the app so that the View title matches our featured view
+                // Basically, we're eliminating the automatically displayed navigationBarTitle and adding our own text element above that matches the rest of our app (the featured view
+                // .navigationBarTitle("All Recipes")
+                .navigationBarHidden(true)
             }
-            .navigationBarTitle("All Recipes")
+            .padding(.leading)
         }
     }
 }
@@ -39,5 +61,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(RecipeModel())
     }
 }
